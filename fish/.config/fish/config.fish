78b3ae1ea49a0523af1b9d08ed083842da4f0fcb
Paul Ouellette <oue.paul18@gmail.com>
2018-03-20 08:25:56 -0400

    Fix missing keybindings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,14 +8,20 @@ function fish_prompt
     printf '%s' (prompt_pwd) ' > '
 end
 
-alias diff "diff --color=auto"
 alias dnfs "dnf search"
 alias dnfu "sudo dnf upgrade"
 alias dnfi "sudo dnf install"
 alias dnfr "sudo dnf remove"
+
+alias diff "diff --color=auto"
 alias bc "bc -lq"
 alias notes "ranger --cmd='set column_ratios 1,2' ~/Documents/notes"
 alias todo "vim ~/Documents/notes/todo ~/Documents/notes/done-list"
+
+function fish_user_key_bindings
+    bind \ef nextd-or-forward-word
+    bind \eb prevd-or-backward-word
+end
 
 if status is-login
     set -gx PATH ~/bin ~/.local/bin $PATH
