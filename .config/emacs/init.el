d0cb488cf33aa06ef96f2f8151bc245c65b03d09
Paul Ouellette <oue.paul18@gmail.com>
2021-07-17 01:36:39 -0400

    Remove emacs

--- .config/emacs/init.el
+++
@@ -1,9 +0,0 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-
-(require 'evil)
-(evil-mode 1)
-
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
